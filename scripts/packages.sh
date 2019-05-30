ebb494d5d294217a7c4504868420e32f17305fd6
Sergey Marunich <marunich.s@gmail.com>
2019-05-30 15:24:48 -0400

    adding az cli

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -30,6 +30,9 @@ if [ "$ID" == "centos" ] || [ "$ID" == "rhel" ]; then
     sudo pip install docker
     sudo ansible-galaxy install avinetworks.avisdk
     sudo ansible-galaxy install avinetworks.aviconfig
+    sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+    sudo sh -c 'echo -e "[azure-cli]\nname=Azure CLI\nbaseurl=https://packages.microsoft.com/yumrepos/azure-cli\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/azure-cli.repo'
+    $cmd azure-cli
 elif [ "$ID" == "kali" ]; then
     echo "*Running for Kali Linux"
     sudo DEBIAN_FRONTEND=noninteractive apt-get -y update
